a8e4db4acd35c4448376ff0084e8bd6c6431497d
Sangram Bankar <sangram.a.bankar@gmail.com>
2017-01-23 16:26:28 -0600

    some changes

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@
 /**
  * configure here
  */
-$from = 'IT WORKER <it@domain.com>';
-$sendTo = 'IT WORKER <it@domain.com>';
+$from = 'sangram.a.bankar@gmail.com';
+$sendTo = 'sangram.a.bankar@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
 $htmlHeader = '';
